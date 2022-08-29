6538c35b1fadc7c6a5b1de3301afc07ad62e0ca2
Ethan <1028716801@qq.com>
2022-08-29 10:39:32 +0800

    更改端口

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 服务器选项
+  server: {
+    port: 8080   // 默认5173，比较难记，可更改为8080
+  }
 })
