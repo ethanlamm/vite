c94bbe27f456d363fa20d93938ab7f3ae5668197
Ethan <1028716801@qq.com>
2022-08-29 11:58:42 +0800

    src别名和静态资源别名的配置

---
+++ src/components/demo1.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>{{ name }}</div>
+    <!-- 1.依据相对路径引用 -->
+    <img src="../assets/images/sqmid1.jfif" alt="">
+    <!-- 2.依据已配置的images文件夹的别名引用 -->
+    <img src="/images/sqmid2.jfif" alt="">
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const name = ref('ethan lam')
+</script>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 引入path
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +13,18 @@ export default defineConfig({
   // 公共基础路径
   base: '/',    // 默认'/'，一般不需要改动
   // 环境选项
-  mode: 'staging'
+  mode: 'staging',
+  // 解析选项
+  resolve: {
+    alias: {
+      // src 别名配置
+      '@': path.resolve(__dirname, './src'),
+
+      // 可设置图片等静态资源文件夹的别名
+      // 静态资源配置注意：
+      // 1.设置key时，前面必须加上'/'
+      // 2.设置value时，必须是字符串，不能用path.resolve处理
+      '/images': '/src/assets/images'
+    }
+  }
 })
